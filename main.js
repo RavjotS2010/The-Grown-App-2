55b2ed7a4ce5091a87ab87b7cb53831d72bb30cd
RavjotS2010 <98565765+RavjotS2010@users.noreply.github.com>
2022-12-27 18:27:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+
+}
+
+
+function setup(){
+    canvas=createCanvas(500,350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500,350);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
+}
+
+function draw(){
+    image(video,0,0,500,350);
+}
+
+
+function Takesnapshot() {
+save("Grown_Up_Picture.png");
+}
+
+
+function modelLoaded() {
+    console.log("poseNet Has been loaded");
+}
+
+function gotPose(results) {
+    if(results.length>0) {
+        console.log(results);
+        console.log("nose x: "+results[0].pose.nose.x);
+        console.log("nose y: "+results[0].pose.nose.y);
+    }
+}
